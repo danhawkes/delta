b92dd3e45b39c9ec29204163cb703de4cd541bb6
Dan Hawkes <dan@danhawkes.co.uk>
2015-12-01 00:22:21 +0000

    remove overlay debug tint

--- app/src/main/java/co/arcs/launcher/ui/overlay/OverlayView.java
+++ app/src/main/java/co/arcs/launcher/ui/overlay/OverlayView.java
@@ -1,12 +1,9 @@
 package co.arcs.launcher.ui.overlay;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.FrameLayout;
 
-import co.arcs.launcher.utils.MotionEventDebug;
 import rx.Observable;
 import rx.subjects.PublishSubject;
 
@@ -14,15 +11,8 @@ public class OverlayView extends FrameLayout {
 
     private PublishSubject<MotionEvent> motionEventsSubject = PublishSubject.create();
 
-    static int[] colors = {Color.GREEN};
-    static int i = 0;
-
     public OverlayView(Context context) {
         super(context);
-        if (i == colors.length) {
-            i = 0;
-        }
-        setBackgroundColor(colors[i++] & 0x40ffffff);
     }
 
     @Override
@@ -37,8 +27,6 @@ public class OverlayView extends FrameLayout {
     }
 
     public Observable<MotionEvent> touchEvents() {
-        return motionEventsSubject.doOnNext(e -> {
-            Log.d("test", MotionEventDebug.toString(e));
-        }).cast(MotionEvent.class);
+        return motionEventsSubject.cast(MotionEvent.class);
     }
 }
